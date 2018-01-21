cc1c26727c00dcbc264ec4de8046b827529805b6
barks1212 <willbarkerwork@gmail.com>
2018-01-21 13:37:52 +0000

    linked js and html, electron app set up

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const electron = require('electron');
+
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/main.html`);
+});
